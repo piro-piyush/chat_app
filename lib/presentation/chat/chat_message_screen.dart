2e919c137150846ee914254a557a5d84e609f4b3
piyush <piyush72717272@gmail.com>
2025-03-10 22:27:53 +0530

    feat: Create ChatMessageScreen
    This commit introduces the `ChatMessageScreen` class. This screen will be responsible for displaying the chat messages.

---
+++ lib/presentation/chat/chat_message_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ChatMessageScreen extends StatelessWidget {
+  const ChatMessageScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
