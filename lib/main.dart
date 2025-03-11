7d89051021c5e05c108ded472e231192daabd7f3
piyush <piyush72717272@gmail.com>
2025-03-11 10:42:53 +0530

    feat: Initialize service locator and remove Firebase from main
    This commit initializes the service locator for dependency injection and removes Firebase initialization from the `main.dart` file. It also moves the main app to `app.dart`.
    
    Key changes:
    
    -   Added `setupServiceLocator()` to `main.dart` to initialize the service locator.
    -   Created `app.dart` with `MyApp`.
    -   Removed Firebase initialization from `main.dart`.
    - Removed imports related to `LoginScreen` and `AppTheme` from `main.dart`.
    - Removed `WidgetsFlutterBinding.ensureInitialized();` from `main.dart`.

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,10 @@
-import 'package:chat_app/config/theme/app_theme.dart';
-import 'package:chat_app/presentation/auth/login_screen.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:chat_app/data/services/service_locator.dart';
 import 'package:flutter/material.dart';
 
-import 'firebase_options.dart';
+import 'app.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await setupServiceLocator();
 
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Chat App',
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme.lightTheme,
-      home: const LoginScreen(),
-    );
-  }
 }
