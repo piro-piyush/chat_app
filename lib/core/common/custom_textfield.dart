850511e799f3918e0df699acb0ea950a93cfcce4
piyush <piyush72717272@gmail.com>
2025-03-11 15:41:54 +0530

    feat: Create custom text field
    This commit introduces a `CustomTextField` widget, which extends the `TextFormField`.
    
    Key changes:
    
    -   Created a new `CustomTextField` widget that allows for customizable text input.
    -   Added parameters for `focusNode`, `obscureText`, and `keyboardType`.
    - Set the controller for the TextFormField.

--- lib/core/common/custom_textfield.dart
+++ lib/core/common/custom_textfield.dart
@@ -28,6 +28,7 @@ class CustomTextField extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      controller: controller,
       focusNode: focusNode,
       obscureText: obscureText,
       keyboardType: keyboardType,
