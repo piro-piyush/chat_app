4f63ed61facbce2945c7b5153f30eab307dc6dfd
piyush <piyush72717272@gmail.com>
2025-03-11 15:41:36 +0530

    feat: Implement signup functionality
    This commit implements the signup functionality for new users.
    
    Key changes:
    
    -   Integrated `AuthCubit` for handling signup logic.
    -   Added form validation for name, username, email, phone, and password fields.
    -   Implemented `createAccount` function to handle account creation.
    - Added a listener to handle the different states of AuthCubit, like authenticated, error and loading.
    - Added the CircularProgressIndicator widget when loading.
    - Replaced `Navigator.pop()` with `getIt<AppRouter>().pop()`
    - Pushed to `HomeScreen` when the user is authenticated
    - Show an error message when an error happens.
    - Added `UiUtils.showWarningSnackBar` method to show validation errors.
    - Renamed `Phone` to `Phone (10 Digit)` in the hintText for a better user experience.

--- lib/presentation/auth/signup_screen.dart
+++ lib/presentation/auth/signup_screen.dart
@@ -1,7 +1,16 @@
+import 'dart:developer';
 import 'package:chat_app/core/common/custom_button.dart';
 import 'package:chat_app/core/common/custom_textfield.dart';
+import 'package:chat_app/core/utils/ui_utils.dart';
+import 'package:chat_app/data/repositories/auth_repository.dart';
+import 'package:chat_app/data/services/service_locator.dart';
+import 'package:chat_app/logic/cubits/auth/auth_cubit.dart';
+import 'package:chat_app/logic/cubits/auth/auth_state.dart';
+import 'package:chat_app/presentation/home/home_screen.dart';
+import 'package:chat_app/router/app_router.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SignupScreen extends StatefulWidget {
   const SignupScreen({super.key});
@@ -100,130 +109,168 @@ class _SignupScreenState extends State<SignupScreen> {
     return null;
   }
 
-  void createAccount() {
+  void createAccount() async {
     FocusScope.of(context).unfocus();
-    if (_formKey.currentState!.validate()) {}
+    if (_formKey.currentState?.validate() ?? false) {
+      try {
+        await getIt<AuthCubit>().signUp(
+          username: usernameController.text,
+          email: emailController.text,
+          fullName: nameController.text,
+          phoneNumber: phoneController.text,
+          password: passwordController.text,
+        );
+      } catch (e) {
+        UiUtils.showErrorSnackBar(context: context, message: e.toString());
+      }
+    } else {
+      UiUtils.showWarningSnackBar(
+        context: context,
+        message: "Form Validation failed",
+      );
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: SafeArea(
-        child: Form(
-          key: _formKey,
-          child: SingleChildScrollView(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Welcome Back",
-                  style: Theme.of(context).textTheme.headlineMedium?.copyWith(
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                SizedBox(height: 10),
-                Text(
-                  "Please fill int the details to continue",
-                  style: Theme.of(
-                    context,
-                  ).textTheme.bodyLarge?.copyWith(color: Colors.grey),
-                ),
-                SizedBox(height: 30),
-                CustomTextField(
-                  controller: nameController,
-                  hintText: "Full Name",
-                  validator: _validateName,
-                  focusNode: _nameFocusNode,
-                  prefixIcon: Icon(Icons.person_outlined),
-                  onFieldSubmitted: (_) => _usernameFocusNode.requestFocus(),
-                ),
-                SizedBox(height: 16),
-                CustomTextField(
-                  controller: usernameController,
-                  hintText: "Username",
-                  focusNode: _usernameFocusNode,
-                  validator: _validateUsername,
-                  prefixIcon: Icon(Icons.alternate_email),
-                  onFieldSubmitted: (_) => _emailFocusNode.requestFocus(),
-                ),
-                SizedBox(height: 16),
-                CustomTextField(
-                  controller: emailController,
-                  hintText: "Email",
-                  focusNode: _emailFocusNode,
-                  validator: _validateEmail,
-                  prefixIcon: Icon(Icons.email_outlined),
-                  onFieldSubmitted: (_) => _phoneFocusNode.requestFocus(),
-                ),
-                SizedBox(height: 16),
-                CustomTextField(
-                  controller: phoneController,
-                  hintText: "Phone",
-                  focusNode: _phoneFocusNode,
-                  validator: _validatePhone,
-                  keyboardType: TextInputType.number,
-                  prefixIcon: Icon(Icons.phone_outlined),
-                  onFieldSubmitted: (_) => _passwordFocusNode.requestFocus(),
-                ),
-                SizedBox(height: 16),
-                CustomTextField(
-                  controller: passwordController,
-                  hintText: "Password",
-                  validator: _validatePassword,
-                  focusNode: _passwordFocusNode,
-                  prefixIcon: Icon(Icons.lock_outlined),
-                  suffixIcon: IconButton(
-                    onPressed: () {
-                      setState(() {
-                        isPasswordVisible = !isPasswordVisible;
-                      });
-                    },
-                    icon: Icon(
-                      isPasswordVisible
-                          ? Icons.visibility_off
-                          : Icons.visibility,
+    return BlocConsumer<AuthCubit, AuthState>(
+      bloc: getIt<AuthCubit>(),
+      listener: (context, state) {
+        if (state.status == AuthStatus.authenticated) {
+          getIt<AppRouter>().pushAndRemoveUntil(HomeScreen());
+        }
+        if (state.status == AuthStatus.error && state.error != null) {
+          UiUtils.showErrorSnackBar(context: context, message: state.error!);
+        }
+      },
+      builder: (context, state) {
+        return Scaffold(
+          appBar: AppBar(),
+          body: SafeArea(
+            child: Form(
+              key: _formKey,
+              child: SingleChildScrollView(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Welcome Back",
+                      style: Theme.of(context).textTheme.headlineMedium
+                          ?.copyWith(fontWeight: FontWeight.bold),
                     ),
-                  ),
-                  obscureText: isPasswordVisible,
-                  onFieldSubmitted: (_) => createAccount(),
-                ),
-                SizedBox(height: 30),
-                CustomButton(
-                  onPressed: () => createAccount(),
-                  text: "Create Account",
-                ),
-                SizedBox(height: 20),
-                Center(
-                  child: RichText(
-                    text: TextSpan(
-                      text: "Already have an account?  ",
-                      style: TextStyle(color: Colors.grey[600]),
-                      children: [
-                        TextSpan(
-                          text: "Login",
-                          style: Theme.of(
-                            context,
-                          ).textTheme.bodyLarge?.copyWith(
-                            color: Theme.of(context).primaryColor,
-                            fontWeight: FontWeight.bold,
-                          ),
-                          recognizer:
-                              TapGestureRecognizer()
-                                ..onTap = () {
-                                  Navigator.pop(context);
-                                },
+                    SizedBox(height: 10),
+                    Text(
+                      "Please fill int the details to continue",
+                      style: Theme.of(
+                        context,
+                      ).textTheme.bodyLarge?.copyWith(color: Colors.grey),
+                    ),
+                    SizedBox(height: 30),
+                    CustomTextField(
+                      controller: nameController,
+                      hintText: "Full Name",
+                      validator: _validateName,
+                      focusNode: _nameFocusNode,
+                      prefixIcon: Icon(Icons.person_outlined),
+                      onFieldSubmitted:
+                          (_) => _usernameFocusNode.requestFocus(),
+                    ),
+                    SizedBox(height: 16),
+                    CustomTextField(
+                      controller: usernameController,
+                      hintText: "Username",
+                      focusNode: _usernameFocusNode,
+                      validator: _validateUsername,
+                      prefixIcon: Icon(Icons.alternate_email),
+                      onFieldSubmitted: (_) => _emailFocusNode.requestFocus(),
+                    ),
+                    SizedBox(height: 16),
+                    CustomTextField(
+                      controller: emailController,
+                      hintText: "Email",
+                      focusNode: _emailFocusNode,
+                      validator: _validateEmail,
+                      prefixIcon: Icon(Icons.email_outlined),
+                      onFieldSubmitted: (_) => _phoneFocusNode.requestFocus(),
+                    ),
+                    SizedBox(height: 16),
+                    CustomTextField(
+                      controller: phoneController,
+                      hintText: "Phone (10 Digit)",
+                      focusNode: _phoneFocusNode,
+                      validator: _validatePhone,
+                      keyboardType: TextInputType.number,
+                      prefixIcon: Icon(Icons.phone_outlined),
+                      onFieldSubmitted:
+                          (_) => _passwordFocusNode.requestFocus(),
+                    ),
+                    SizedBox(height: 16),
+                    CustomTextField(
+                      controller: passwordController,
+                      hintText: "Password",
+                      validator: _validatePassword,
+                      focusNode: _passwordFocusNode,
+                      prefixIcon: Icon(Icons.lock_outlined),
+                      suffixIcon: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            isPasswordVisible = !isPasswordVisible;
+                          });
+                        },
+                        icon: Icon(
+                          isPasswordVisible
+                              ? Icons.visibility_off
+                              : Icons.visibility,
                         ),
-                      ],
+                      ),
+                      obscureText: isPasswordVisible,
+                      onFieldSubmitted: (_) => createAccount(),
                     ),
-                  ),
+                    SizedBox(height: 30),
+                    CustomButton(
+                      onPressed: createAccount,
+                      text: "Create Account",
+                      child:
+                          state.status == AuthStatus.loading
+                              ? CircularProgressIndicator(color: Colors.white)
+                              : Text(
+                                "Create Account",
+                                style: TextStyle(color: Colors.white),
+                              ),
+                    ),
+                    SizedBox(height: 20),
+                    Center(
+                      child: RichText(
+                        text: TextSpan(
+                          text: "Already have an account?  ",
+                          style: TextStyle(color: Colors.grey[600]),
+                          children: [
+                            TextSpan(
+                              text: "Login",
+                              style: Theme.of(
+                                context,
+                              ).textTheme.bodyLarge?.copyWith(
+                                color: Theme.of(context).primaryColor,
+                                fontWeight: FontWeight.bold,
+                              ),
+                              recognizer:
+                                  TapGestureRecognizer()
+                                    ..onTap = () {
+                                      getIt<AppRouter>().pop();
+                                    },
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
