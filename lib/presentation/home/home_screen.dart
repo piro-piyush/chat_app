1972e09e690fd487b37f1dd88a2dc90f9fc3acf2
piyush <piyush72717272@gmail.com>
2025-03-11 15:40:19 +0530

    feat: Implement HomeScreen with contact list and logout
    This commit implements the `HomeScreen` widget, which displays a list of contacts and provides a logout button.
    
    Key changes:
    
    -   Added a `StatefulWidget` implementation for `HomeScreen`.
    -   Initialized `ContactRepository` in the `initState`.
    -   Implemented `_showContactList` to display a modal bottom sheet with the contacts.
    -   Used `FutureBuilder` to handle asynchronous contact fetching.
    -   Displayed a list of contacts using `ListView.builder`.
    -   Added a `logout` button to the `AppBar` that calls `signOut` on `AuthCubit`.
    -   Added a floating action button that opens the contact list modal.
    -   Displays a message to the user if the user is authenticated.

--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -1,10 +1,95 @@
+import 'package:chat_app/data/repositories/contact_repository.dart';
+import 'package:chat_app/data/services/service_locator.dart';
+import 'package:chat_app/logic/cubits/auth/auth_cubit.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late ContactRepository _contactRepository;
+
+  @override
+  void initState() {
+    _contactRepository = getIt<ContactRepository>();
+    super.initState();
+  }
+
+  void _showContactList(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return Container(
+          width: double.infinity,
+          padding: EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Contacts",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              Expanded(
+                child: FutureBuilder(
+                  future: _contactRepository.getRegisteredContacts(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasError) {
+                      return Center(child: Text("Error : ${snapshot.error}"));
+                    }
+                    if (!snapshot.hasData) {
+                      return Center(child: CircularProgressIndicator());
+                    }
+                    final contacts = snapshot.data!;
+                    if (contacts.isEmpty) {
+                      return Center(child: Text("No Contacts Found"));
+                    }
+                    return ListView.builder(
+                      itemBuilder: (contxt, int) {
+                        final Map<String, dynamic> contact = contacts[int];
+                        return ListTile(
+                          leading: CircleAvatar(
+                            backgroundColor: Theme.of(
+                              context,
+                            ).primaryColor.withValues(alpha: 0.1),
+                            child: Text(contact["name"][0].toUpperCase()),
+                          ),
+                          title: Text(contact['name']),
+                          subtitle: Text(contact['phoneNumber']),
+                        );
+                      },
+                      itemCount: contacts.length,
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Chats"),
+        actions: [
+          IconButton(
+            onPressed: () => getIt<AuthCubit>().signOut(),
+            icon: Icon(Icons.logout),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _showContactList(context),
+        child: Icon(Icons.chat, color: Colors.white),
+      ),
+      body: Center(child: Text("I am Authenticated")),
+    );
   }
 }
