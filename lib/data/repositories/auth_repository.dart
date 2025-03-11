c8c80705a77eb3e57022f8d8ad545a2f936717e2
piyush <piyush72717272@gmail.com>
2025-03-11 15:41:16 +0530

    feat: Implement user signup, signin, and data management with Firebase
    This commit introduces core authentication and user data management functionalities using Firebase.
    
    Key changes:
    
    -   Implemented `signUp` functionality in `AuthRepository` using `createUserWithEmailAndPassword`.
    -   Implemented `signIn` functionality in `AuthRepository` using `signInWithEmailAndPassword`.
    -   Implemented `signOut` functionality in `AuthRepository` using `signOut`.
    -   Added logic to check for existing username, email, and phone number during signup.
    -   Implemented `saveUser` in `AuthRepository` to store user data in Firestore.
    -   Implemented `getUserData` in `AuthRepository` to retrieve user data from Firestore.
    -   Implemented `checkIfEmailExists`, `checkIfPhoneExists`, and `checkIfUserNameExists` for data validation.
    -   Implemented `authStateChanges` stream in `AuthRepository` to observe authentication status changes.
    - Handle errors in each method.

--- lib/data/repositories/auth_repository.dart
+++ lib/data/repositories/auth_repository.dart
@@ -1,5 +1,140 @@
+import 'dart:developer';
+
+import 'package:chat_app/data/models/user_model.dart';
 import 'package:chat_app/data/services/base_repository.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthRepository extends BaseRepository {
+  Stream<User?> get authStateChanges => auth.authStateChanges();
 
+  Future<UserModel> signUp({
+    required String username,
+    required String email,
+    required String fullName,
+    required String phoneNumber,
+    required String password,
+  }) async {
+    try {
+      final userNameExists = await checkIfUserNameExists(userName: username);
+      if (userNameExists) {
+        throw "Username already exists";
+      }
+      final emailExists = await checkIfEmailExists(email: email);
+      if (emailExists) {
+        throw "Email already exists";
+      }
+      final phoneExists = await checkIfPhoneExists(phoneNumber: phoneNumber);
+      if (phoneExists) {
+        throw "Phone number already exists";
+      }
+
+      final userCredential = await auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      if (userCredential.user == null) {
+        throw "Failed to create user";
+      }
+      // Create a user model to save in db
+      final userModel = UserModel(
+        uid: userCredential.user!.uid,
+        username: username,
+        fullName: fullName,
+        email: email,
+        phoneNumber: phoneNumber,
+      );
+      saveUser(userModel);
+      return userModel;
+    } catch (e) {
+      log(e.toString());
+      rethrow;
+    }
+  }
+
+  Future<UserModel> signIn({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final userCredential = await auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      if (userCredential.user == null) {
+        throw "User not found";
+      }
+      final userData = await getUserData(userCredential.user!.uid);
+      return userData;
+    } catch (e) {
+      log(e.toString());
+      rethrow;
+    }
+  }
+
+  Future<void> saveUser(UserModel userModel) async {
+    try {
+      await db.collection("Users").doc(userModel.uid).set(userModel.toMap());
+    } catch (e) {
+      throw "Failed to save user data in Firestore";
+    }
+  }
+
+  Future<UserModel> getUserData(String uid) async {
+    try {
+      DocumentSnapshot doc = await db.collection("Users").doc(uid).get();
+      if (!doc.exists) {
+        throw "User data not found";
+      }
+      return UserModel.fromFirestore(doc);
+    } catch (e) {
+      throw "Failed to save user data in Firestore";
+    }
+  }
+
+  Future<void> signOut() async {
+    try {
+      await auth.signOut();
+    } catch (e) {
+      throw "Failed to sign out";
+    }
+  }
+
+  Future<bool> checkIfEmailExists({required String email}) async {
+    try {
+      final methods = await auth.fetchSignInMethodsForEmail(email);
+      return methods.isNotEmpty;
+    } catch (e) {
+      print("Failed to check email");
+      return false;
+    }
+  }
+
+  Future<bool> checkIfPhoneExists({required String phoneNumber}) async {
+    try {
+      final querySnapshot =
+          await db
+              .collection("Users")
+              .where("phoneNumber", isEqualTo: phoneNumber)
+              .get();
+      return querySnapshot.docs.isNotEmpty;
+    } catch (e) {
+      print("Failed to check Phone Number");
+      return false;
+    }
+  }
+
+  Future<bool> checkIfUserNameExists({required String userName}) async {
+    try {
+      final querySnapshot =
+          await db
+              .collection("Users")
+              .where("username", isEqualTo: userName)
+              .get();
+      return querySnapshot.docs.isNotEmpty;
+    } catch (e) {
+      print("Failed to check userName");
+      return false;
+    }
+  }
 }
