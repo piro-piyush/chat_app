8c3dc6a4ecdd7d88a14cd3277bd158f9acc6a944
piyush <piyush72717272@gmail.com>
2025-03-11 15:41:07 +0530

    feat: Add methods to access current user and authentication status
    This commit introduces new methods to the `BaseRepository` for easier access to the current user and their authentication status.
    
    Key changes:
    
    -   Added `currentUser` getter that returns the currently signed-in user from `FirebaseAuth`.
    -   Added `uid` getter that returns the user's ID or an empty string if no user is signed in.
    -   Added `isAuthenticated` getter that returns a boolean indicating if a user is currently signed in.

--- lib/data/services/base_repository.dart
+++ lib/data/services/base_repository.dart
@@ -4,4 +4,10 @@ import 'package:firebase_auth/firebase_auth.dart';
 abstract class BaseRepository {
   final FirebaseAuth auth = FirebaseAuth.instance;
   final FirebaseFirestore db = FirebaseFirestore.instance;
+
+  User? get currentUser => auth.currentUser;
+
+  String get uid => currentUser?.uid ?? "";
+
+  bool get isAuthenticated => currentUser != null;
 }
