3fad2a1d5e422380126ee5cdf98e65aa8e8129cc
piyush <piyush72717272@gmail.com>
2025-03-12 19:48:39 +0530

    feat: Display chat rooms in Home Screen
    This commit introduces the display of chat rooms in the `HomeScreen`.
    
    Key changes:
    
    -   Added `ChatRepository` to fetch chat rooms.
    -   Added `currentUserId` from `AuthCubit` to identify the current user.
    -   Used `StreamBuilder` to get the list of chat rooms from `ChatRepository` based on currentUserId.
    -   Implemented `ChatListTile` to display each chat room in the list.
    -   Navigated to `ChatMessageScreen` when a chat room is tapped.
    - Refactored the previous logic to display the contact list in a dialog when the user press on the button.

--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -1,6 +1,10 @@
+import 'package:chat_app/data/repositories/chat_repository.dart';
 import 'package:chat_app/data/repositories/contact_repository.dart';
 import 'package:chat_app/data/services/service_locator.dart';
 import 'package:chat_app/logic/cubits/auth/auth_cubit.dart';
+import 'package:chat_app/presentation/chat/chat_message_screen.dart';
+import 'package:chat_app/router/app_router.dart';
+import 'package:chat_app/widgets/chat_list_tile.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -12,10 +16,15 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   late ContactRepository _contactRepository;
+  late ChatRepository _chatRepository;
+
+  late String currentUserId;
 
   @override
   void initState() {
     _contactRepository = getIt<ContactRepository>();
+    _chatRepository = getIt<ChatRepository>();
+    currentUserId = getIt<AuthCubit>().state.user!.uid;
     super.initState();
   }
 
@@ -51,6 +60,13 @@ class _HomeScreenState extends State<HomeScreen> {
                       itemBuilder: (contxt, int) {
                         final Map<String, dynamic> contact = contacts[int];
                         return ListTile(
+                          onTap:
+                              () => getIt<AppRouter>().push(
+                                ChatMessageScreen(
+                                  receiverId: contact["id"],
+                                  receiverName: contact['name'],
+                                ),
+                              ),
                           leading: CircleAvatar(
                             backgroundColor: Theme.of(
                               context,
@@ -89,7 +105,44 @@ class _HomeScreenState extends State<HomeScreen> {
         onPressed: () => _showContactList(context),
         child: Icon(Icons.chat, color: Colors.white),
       ),
-      body: Center(child: Text("I am Authenticated")),
+      body: StreamBuilder(
+        stream: _chatRepository.getChatRooms(userId: currentUserId),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Center(child: Text("Error : ${snapshot.error}"));
+          }
+          if (!snapshot.hasData) {
+            return Center(child: CircularProgressIndicator());
+          }
+          final chats = snapshot.data!;
+          if (chats.isEmpty) {
+            return Center(child: Text("No Chats Found"));
+          }
+          return ListView.builder(
+            itemCount: chats.length,
+            itemBuilder: (context, index) {
+              final chat = chats[index];
+              return ChatListTile(
+                chat: chat,
+                currentUserId: currentUserId,
+                onTap: () {
+                  final otherUserId = chat.participants.firstWhere(
+                    (id) => id != currentUserId,
+                  );
+                  final otherUserName =
+                      chat.participantsName![otherUserId] ?? "Unknown";
+                  getIt<AppRouter>().push(
+                    ChatMessageScreen(
+                      receiverName: otherUserName,
+                      receiverId: otherUserId,
+                    ),
+                  );
+                },
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
