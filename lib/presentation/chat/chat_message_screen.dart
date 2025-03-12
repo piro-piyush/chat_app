22f99b6ff53c80f2dce0e5600977c412507b5c79
piyush <piyush72717272@gmail.com>
2025-03-12 19:48:30 +0530

    feat: Implement chat message screen
    This commit introduces the `ChatMessageScreen` widget, which displays a chat interface for messaging between users.
    
    Key changes:
    
    -   Created `ChatMessageScreen` to display chat messages with a specified receiver.
    -   Added `ChatCubit` for managing chat data and interactions.
    -   Implemented sending and receiving messages.
    -   Implemented a UI for typing and sending messages.
    -   Used `ListView.builder` to display a dynamic list of messages.
    -   Implemented `MessageBubble` widget for individual message rendering.
    -   Implemented `BlocBuilder` to dynamically update the UI based on the chat state.
    -   Added basic error and loading states handling.
    -   Implemented `DateFormat` to display message timestamps.
    -   Added input decoration for message text field.
    -   Added `AppBar` with receiver's name and online status.
    -   Added styling to distinguish messages from the current user and the receiver.
    - Added basic handling for sending error.
    - Added chat enter logic when user enter the screen.

--- lib/presentation/chat/chat_message_screen.dart
+++ lib/presentation/chat/chat_message_screen.dart
@@ -1,10 +1,227 @@
+import 'package:chat_app/core/utils/ui_utils.dart';
+import 'package:chat_app/data/models/chat_message.dart';
+import 'package:chat_app/data/services/service_locator.dart';
+import 'package:chat_app/logic/cubits/chat/chat_cubit.dart';
+import 'package:chat_app/logic/cubits/chat/chat_state.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
 
-class ChatMessageScreen extends StatelessWidget {
-  const ChatMessageScreen({super.key});
+class ChatMessageScreen extends StatefulWidget {
+  final String receiverId;
+  final String receiverName;
+
+  const ChatMessageScreen({
+    super.key,
+    required this.receiverId,
+    required this.receiverName,
+  });
+
+  @override
+  State<ChatMessageScreen> createState() => _ChatMessageScreenState();
+}
+
+class _ChatMessageScreenState extends State<ChatMessageScreen> {
+  final TextEditingController messageController = TextEditingController();
+  late final ChatCubit _chatCubit;
+
+  @override
+  void initState() {
+    _chatCubit = getIt<ChatCubit>();
+    _chatCubit.enterChat(receiverId: widget.receiverId);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    messageController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _sendMessage() async {
+    try {
+      final message = messageController.text.trim();
+      messageController.clear();
+      if (message.isNotEmpty) {
+        await _chatCubit.sendMessage(
+          receiverId: widget.receiverId,
+          content: message,
+        );
+      }
+    } catch (e) {
+      if (!mounted) return;
+      UiUtils.showErrorSnackBar(
+        context: context,
+        message: "Failed to send Message : ${e.toString()}",
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          spacing: 12,
+          children: [
+            CircleAvatar(
+              backgroundColor: Theme.of(
+                context,
+              ).primaryColor.withValues(alpha: 0.1),
+              child: Text(widget.receiverName[0].toUpperCase()),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(widget.receiverName),
+                Text(
+                  "Online",
+                  style: TextStyle(color: Colors.green, fontSize: 12),
+                ),
+              ],
+            ),
+          ],
+        ),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.more_vert_outlined)),
+        ],
+      ),
+      body: BlocBuilder<ChatCubit, ChatState>(
+        bloc: _chatCubit,
+        builder: (context, state) {
+          if (state.status == ChatStatus.loading) {
+            return Center(child: CircularProgressIndicator());
+          }
+          if (state.status == ChatStatus.error) {
+            return Center(
+              child: Text("Error : ${state.error ?? "Something went wrong"}"),
+            );
+          }
+          return Column(
+            children: [
+              Expanded(
+                child: ListView.builder(
+                  reverse: true,
+                  itemCount: state.messages.length,
+                  itemBuilder: (context, index) {
+                    final message = state.messages[index];
+                    final bool isMe =
+                        message.senderId == _chatCubit.currentUserId;
+                    return MessageBubble(message: message, isMe: isMe);
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    Row(
+                      spacing: 8,
+                      children: [
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(Icons.emoji_emotions),
+                        ),
+                        Expanded(
+                          child: TextField(
+                            controller: messageController,
+                            minLines: 1,
+                            maxLines: 3,
+                            textCapitalization: TextCapitalization.sentences,
+                            keyboardType: TextInputType.multiline,
+                            decoration: InputDecoration(
+                              hintText: "Type a message",
+                              filled: true,
+                              contentPadding: EdgeInsets.symmetric(
+                                horizontal: 12,
+                                vertical: 8,
+                              ),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(24),
+                                borderSide: BorderSide.none,
+                              ),
+                              fillColor: Theme.of(context).cardColor,
+                            ),
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () async => _sendMessage(),
+                          icon: Icon(
+                            Icons.send,
+                            color: Theme.of(context).primaryColor,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
+}
+
+class MessageBubble extends StatelessWidget {
+  final ChatMessage message;
+  final bool isMe;
+
+  const MessageBubble({super.key, required this.message, required this.isMe});
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: isMe ? Alignment.centerRight : Alignment.centerLeft,
+      child: Container(
+        decoration: BoxDecoration(
+          color:
+              isMe
+                  ? Theme.of(context).primaryColor.withValues(alpha: 0.9)
+                  : Theme.of(context).primaryColor.withValues(alpha: 0.1),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+        margin: EdgeInsets.only(
+          left: isMe ? 64 : 8,
+          right: isMe ? 8 : 64,
+          bottom: 4,
+        ),
+        child: Column(
+          crossAxisAlignment:
+              isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+          children: [
+            Text(
+              message.content,
+              style: TextStyle(color: isMe ? Colors.white : Colors.black),
+            ),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              spacing: 12,
+              children: [
+                Text(
+                  DateFormat('h:mm a').format(message.timestamp.toDate()),
+                  style: TextStyle(
+                    color: isMe ? Colors.white : Colors.black,
+                    fontSize: 11,
+                  ),
+                ),
+                Icon(
+                  Icons.done_all,
+                  color:
+                      message.status == MessageStatus.read
+                          ? Colors.white
+                          : Colors.blue,
+                  size: 14,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
